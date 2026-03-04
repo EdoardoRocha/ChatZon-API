222a0653458555221bf42bb7e31c71062d101ffc
EdoardoRocha <pazedoardo@gmail.com>
2026-03-04 14:58:38 -0300

    Initializing addition websocket

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/api/messages", MessageRoutes);
 app.use("/api/conversations", ConversationRoutes);
 
 const environment = process.env.NODE_ENV;
-const port = process.env.port;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`Servidor de ${environment} rodando na porta ${port}`);
 });
